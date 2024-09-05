90edd8c036410582d36108713ec0ec36db2aa7ea
kurokeita <kuro.keita94@gmail.com>
2024-09-05 15:00:25 +0700

    preload some linters

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -92,4 +92,68 @@ require("lazy").setup({
       end
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        -- Linter
+        "pylint",
+        "cmakelang",
+        "cmakelint",
+        "commitlint",
+        "eslint-d",
+        "jsonlint",
+        "markuplint",
+        "markdownlint",
+        "markdownlint-cli2",
+        "shellcheck",
+        "sqlfluff",
+        -- LSP
+        "intelephense",
+        "json-lsp",
+        "lua-language-server",
+        "eslint-lsp",
+        "bash-language-server",
+        "prisma-language-server",
+        "yaml-language-server",
+        "dockerfile-language-server",
+        "docker-compose-language-service",
+        "prisma-language-server",
+        "vtsls",
+        -- Formatter
+        "prettier",
+        "goimports",
+        "stylua",
+        "shfmt",
+        "markdown-toc",
+      },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+  
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end)
+    end,
+  }
 })
